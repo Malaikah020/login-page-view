dfa4191ad9c7347d8311d16c4ddd5e14b1b0102b
Malaikah Hafeez <Malaikah Hafeez>
2022-01-07 14:29:04 +0000

    Initial Commit

---
+++ EvalTrain/EvalTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvalTrainApp.swift
+//  EvalTrain
+//
+//  Created by Malaikah Hafeez on 07/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct EvalTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
